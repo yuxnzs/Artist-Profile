cb02f1d8502699efa1013076c784a075fb901b7e
yuxnzs <yuchengliu0429@gmail.com>
2024-09-03 00:12:20 +0800

    Implement artist card and row components for homepage genre selection

---
+++ lib/components/artist_card.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:artist_profile/models/artist.dart';
+import 'package:artist_profile/utility/custom_colors.dart';
+
+class ArtistCard extends StatelessWidget {
+  final Artist artist;
+
+  const ArtistCard({super.key, required this.artist});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 160,
+      height: 180,
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: CustomColors.background,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Artist Image
+          ClipOval(
+            child: Image.network(
+              artist.image,
+              width: 100,
+              height: 100,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(height: 10),
+
+          // Artist Name
+          Text(
+            artist.name,
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 1,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 3),
+
+          // Followers
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(
+                Icons.people,
+                size: 18,
+                color: Colors.grey,
+              ),
+              const SizedBox(width: 3),
+              Text(
+                NumberFormat.compact().format(artist.followers),
+                style: const TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/components/artist_row.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:artist_profile/models/artist.dart';
+import 'package:artist_profile/components/artist_card.dart';
+
+class ArtistRow extends StatelessWidget {
+  final List<Artist> artists;
+
+  const ArtistRow({
+    super.key,
+    required this.artists,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 190,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          const SizedBox(width: 20), // Visual spacing
+          ...artists.map((artist) {
+            return Padding(
+              padding: const EdgeInsets.only(right: 25),
+              child: ArtistCard(artist: artist),
+            );
+          }),
+        ],
+      ),
+    );
+  }
+}
