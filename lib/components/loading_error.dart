00acca0629657c6bbed3c5adc0e83ab2dde1ac50
yuxnzs <yuchengliu0429@gmail.com>
2024-09-05 02:04:41 +0800

    Add LoadingError component to display error message and retry button when API fetch fails

---
+++ lib/components/loading_error.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class LoadingError extends StatelessWidget {
+  final Function() onRetry;
+
+  const LoadingError({super.key, required this.onRetry});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 180,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("Failed to load recommendations. Please try again."),
+          const SizedBox(height: 10),
+          OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.red,
+              side: const BorderSide(color: Colors.red),
+            ),
+            onPressed: onRetry,
+            child: const Text("Retry"),
+          ),
+        ],
+      ),
+    );
+  }
+}
