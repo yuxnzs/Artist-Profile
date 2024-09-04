72e085f1fed2dfd9d986d3ac5e4ad6cc40f06343
yuxnzs <yuchengliu0429@gmail.com>
2024-09-04 18:32:29 +0800

    Add extra top margin for better visual spacing on homepage

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -32,7 +32,7 @@ class _HomepageState extends State<Homepage> {
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Container(
-                margin: const EdgeInsets.only(top: 10, left: 20, right: 20),
+                margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
                 child: const Column(
                   children: [
                     // Header Text
