f4b8a314a65ab7c89e2bcca024a21ebde6600de9
yuxnzs <yuchengliu0429@gmail.com>
2024-09-09 23:48:11 +0800

    Refactor to replace SingleChildScrollView with ListView and StatefulWidget with StatelessWidget

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -6,14 +6,9 @@ import 'package:artist_profile/components/homepage_banner.dart';
 import 'package:artist_profile/components/genre_section.dart';
 import 'package:artist_profile/components/artist_content.dart';
 
-class Homepage extends StatefulWidget {
+class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
-  @override
-  State<Homepage> createState() => _HomepageState();
-}
-
-class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     final apiService = context.watch<APIService>();
@@ -22,44 +17,41 @@ class _HomepageState extends State<Homepage> {
       body: SafeArea(
         top: true,
         bottom: false,
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              // Header Text
-              const SizedBox(height: 20),
-              const HeadingText(text: "Artists Profile", fontSize: 25),
-              const SizedBox(height: 15),
+        child: ListView(
+          children: [
+            // Header Text
+            const SizedBox(height: 20),
+            const HeadingText(text: "Artists Profile", fontSize: 25),
+            const SizedBox(height: 15),
 
-              // Banner
-              const HomepageBanner(),
-              const SizedBox(height: 30),
+            // Banner
+            const HomepageBanner(),
+            const SizedBox(height: 30),
 
-              // Global Top Artists Section
-              const HeadingText(text: "Global Top Artists Today"),
-              const SizedBox(height: 15),
-              ArtistContent(
-                artists: apiService.globalTopArtists,
-                apiFunction: apiService.getGlobalTopArtists,
-              ),
-              const SizedBox(height: 30),
+            // Global Top Artists Section
+            const HeadingText(text: "Global Top Artists Today"),
+            const SizedBox(height: 15),
+            ArtistContent(
+              artists: apiService.globalTopArtists,
+              apiFunction: apiService.getGlobalTopArtists,
+            ),
+            const SizedBox(height: 30),
 
-              // USA Top Artists Section
-              const HeadingText(text: "USA Top Artists Today"),
-              const SizedBox(height: 15),
-              ArtistContent(
-                artists: apiService.usTopArtists,
-                apiFunction: apiService.getUSTopArtists,
-              ),
-              const SizedBox(height: 30),
+            // USA's Top Artists Section
+            const HeadingText(text: "USA Top Artists Today"),
+            const SizedBox(height: 15),
+            ArtistContent(
+              artists: apiService.usTopArtists,
+              apiFunction: apiService.getUSTopArtists,
+            ),
+            const SizedBox(height: 30),
 
-              // Recommended Genres Section
-              const HeadingText(text: "Recommended Genres"),
-              const SizedBox(height: 15),
-              const GenreSection(),
-              const SizedBox(height: 30),
-            ],
-          ),
+            // Recommended Genres Section
+            const HeadingText(text: "Recommended Genres"),
+            const SizedBox(height: 15),
+            const GenreSection(),
+            const SizedBox(height: 20),
+          ],
         ),
       ),
     );
