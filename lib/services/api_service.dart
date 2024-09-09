82df6e893f5527c002b14aa30aa0b5d71abab46c
yuxnzs <yuchengliu0429@gmail.com>
2024-09-09 23:53:48 +0800

    Implement API calls for Global, Taiwan, and USA top artists in APIService

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -7,20 +7,49 @@ import 'package:artist_profile/models/artist.dart';
 class APIService with ChangeNotifier {
   final String baseUrl = dotenv.get('BASE_URL');
   List<Artist> recommendedArtists = [];
+  List<Artist> globalTopArtists = [];
+  List<Artist> taiwanTopArtists = [];
+  List<Artist> usTopArtists = [];
 
-  Future<void> getRecommendations() async {
-    final response = await http.get(Uri.parse('$baseUrl/recommendations'));
+  // Fetch artists from the API based on the endpoint
+  Future<List<Artist>> _fetchArtists(String endpoint) async {
+    final response = await http.get(Uri.parse('$baseUrl$endpoint'));
+
+    // Check if the response is successful
     if (response.statusCode == 200) {
+      // Parse the JSON response
       final List<dynamic> jsonData = jsonDecode(response.body);
 
       // Convert each JSON object to an Artist object
-      recommendedArtists = jsonData.map((artist) {
-        return Artist.fromJson(artist);
-      }).toList();
-
-      notifyListeners();
+      return jsonData.map((artist) => Artist.fromJson(artist)).toList();
     } else {
-      throw Exception('Failed to load recommendations');
+      throw Exception('Failed to load artists from $endpoint');
     }
   }
+
+  // Fetch artists for a specific category
+  Future<void> fetchArtistsForCategory(
+      String endpoint, List<Artist> targetList) async {
+    final artists = await _fetchArtists(endpoint);
+    targetList.clear();
+    targetList.addAll(artists);
+
+    notifyListeners();
+  }
+
+  // Fetch recommended artists
+  Future<void> getRecommendations() =>
+      fetchArtistsForCategory('/recommendations', recommendedArtists);
+
+  // Fetch global top artists
+  Future<void> getGlobalTopArtists() =>
+      fetchArtistsForCategory('/spotify-artists/global', globalTopArtists);
+
+  // Fetch US top artists
+  Future<void> getUSTopArtists() =>
+      fetchArtistsForCategory('/spotify-artists/us', usTopArtists);
+
+  // Fetch Taiwan top artists
+  Future<void> getTaiwanTopArtists() =>
+      fetchArtistsForCategory('/spotify-artists/tw', taiwanTopArtists);
 }
