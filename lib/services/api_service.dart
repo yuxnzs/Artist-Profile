7ae404c51853841b118a15d40b6aaad3ad120527
yuxnzs <yuchengliu0429@gmail.com>
2024-09-23 22:38:55 +0800

    Implement API call and refactor _fetchArtistsData to a generic function for ArtistBioPage

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
@@ -11,32 +12,45 @@ class APIService with ChangeNotifier {
   List<Artist> taiwanTopArtists = [];
   List<Artist> usaTopArtists = [];
 
-  // Fetch artists from the API based on the endpoint
-  Future<List<Artist>> _fetchArtists(String endpoint) async {
+  // Fetch data from the API based on the endpoint
+  Future<dynamic> _fetchArtistsData<T>(
+      String endpoint, T Function(Map<String, dynamic>) fromJson) async {
     try {
       final response = await http.get(Uri.parse('$baseUrl$endpoint'));
 
       // Check if the response is successful
       if (response.statusCode == 200) {
-        // Parse the JSON response
-        final List<dynamic> jsonData = jsonDecode(response.body);
+        // Check if the response body is empty (response.body is a string)
+        if (response.body.isEmpty) {
+          log("Response body is empty");
+          return null;
+        }
 
-        // Convert each JSON object to an Artist object
-        return jsonData.map((artist) => Artist.fromJson(artist)).toList();
+        // Parse the JSON response
+        final dynamic jsonData = jsonDecode(response.body);
+
+        // Convert each JSON object to the designated type object
+        if (jsonData is List) {
+          // List of Artist objects, List<Artist>
+          return jsonData.map((item) => fromJson(item)).toList();
+        } else {
+          // An Artist or ArtistBio object
+          return fromJson(jsonData);
+        }
       } else {
         throw Exception(
-            'Failed to load artists from $endpoint. Status code: ${response.statusCode}');
+            'Failed to load data from $endpoint. Status code: ${response.statusCode}');
       }
     } catch (e) {
       // Network error or other exceptions
-      throw Exception('Error fetching artists from $endpoint: $e');
+      throw Exception('Error fetching data from $endpoint: $e');
     }
   }
 
   // Fetch artists for a specific category
   Future<void> fetchArtistsForCategory(
       String endpoint, List<Artist> targetList) async {
-    final artists = await _fetchArtists(endpoint);
+    final artists = await _fetchArtistsData<Artist>(endpoint, Artist.fromJson);
     targetList.clear();
     targetList.addAll(artists);
 
@@ -58,4 +72,31 @@ class APIService with ChangeNotifier {
   // Fetch Taiwan top artists
   Future<void> getTaiwanTopArtists() =>
       fetchArtistsForCategory('/spotify-artists/tw', taiwanTopArtists);
+
+  // Fetch artist data for ArtistBio page
+  Future<dynamic> getArtistData({
+    required String artistName,
+    required bool includeSpotifyInfo,
+  }) async {
+    try {
+      if (includeSpotifyInfo) {
+        // includeSpotifyInfo is true, return Artist
+        final Artist? artist = await _fetchArtistsData<Artist>(
+          '/artist-bio/$artistName?includeSpotifyInfo=$includeSpotifyInfo',
+          Artist.fromJson,
+        );
+        return artist;
+      } else {
+        // includeSpotifyInfo is false, return ArtistBio
+        final ArtistBio? artistBio = await _fetchArtistsData<ArtistBio>(
+          '/artist-bio/$artistName?includeSpotifyInfo=$includeSpotifyInfo',
+          // Handle nested JSON, extract bio
+          (json) => ArtistBio.fromJson(json['bio']),
+        );
+        return artistBio;
+      }
+    } catch (e) {
+      throw Exception('$e');
+    }
+  }
 }
