f16a70b0a8cbd2fbbd4c27762c97bbabbfa85f01
yuxnzs <yuchengliu0429@gmail.com>
2024-09-23 22:46:11 +0800

    Update ArtistCard to navigate to ArtistBioPage on tap

--- lib/components/artist_card.dart
+++ lib/components/artist_card.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
 import 'package:artist_profile/models/artist.dart';
 import 'package:artist_profile/utility/custom_colors.dart';
 import 'package:artist_profile/components/artist_image.dart';
+import 'package:artist_profile/pages/artist_bio_page.dart';
 
 class ArtistCard extends StatelessWidget {
   final Artist artist;
@@ -11,55 +13,69 @@ class ArtistCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 160,
-      height: 180,
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: CustomColors.background,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // Artist Image
-          ArtistImage(imageUrl: artist.image),
-          const SizedBox(height: 10),
-
-          // Artist Name
-          Text(
-            artist.name,
-            textAlign: TextAlign.center,
-            overflow: TextOverflow.ellipsis,
-            maxLines: 1,
-            style: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          CupertinoPageRoute(
+            builder: (context) => ArtistBioPage(
+              artistName: artist.name,
+              apiIncludeSpotifyInfo: false,
+              passedArtist: artist,
             ),
           ),
-          const SizedBox(height: 3),
+        );
+      },
+      child: Container(
+        width: 160,
+        height: 180,
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: CustomColors.background,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Artist Image
+            ArtistImage(imageUrl: artist.image ?? ''),
+            const SizedBox(height: 10),
 
-          // Followers
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Icon(
-                Icons.people,
-                size: 18,
-                color: Colors.grey,
+            // Artist Name
+            Text(
+              artist.name,
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(width: 3),
-              Text(
-                NumberFormat.compact().format(artist.followers),
-                style: const TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
+            ),
+            const SizedBox(height: 3),
+
+            // Followers
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.people,
+                  size: 18,
                   color: Colors.grey,
                 ),
-              ),
-            ],
-          ),
-        ],
+                const SizedBox(width: 3),
+                Text(
+                  NumberFormat.compact().format(artist.followers),
+                  style: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
