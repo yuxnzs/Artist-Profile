818176dff3a2831f04e2f097d30f21391a098311
yuxnzs <yuchengliu0429@gmail.com>
2024-11-29 23:21:49 +0800

    Add bottomNavBarHeight and searchPageMargin in AppConstants

--- lib/utility/app_constants.dart
+++ lib/utility/app_constants.dart
@@ -1,4 +1,6 @@
 class AppConstants {
   static const double globalMargin = 24;
   static const double artistBioPageMargin = 20;
+  static const double bottomNavBarHeight = 50;
+  static const double searchPageMargin = 18;
 }
