8aa71ffb62100e4cba1f89fef5e7e1c437c5303b
yuxnzs <yuchengliu0429@gmail.com>
2024-11-29 19:47:06 +0800

    Add isNoData and artistName parameters for detailed error messages in LoadingError

--- lib/components/common/loading_error.dart
+++ lib/components/common/loading_error.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 
 class LoadingError extends StatelessWidget {
   final Function() onRetry;
+  final bool isNoData;
+  final String artistName;
 
-  const LoadingError({super.key, required this.onRetry});
+  const LoadingError({
+    super.key,
+    required this.onRetry,
+    this.isNoData = false,
+    this.artistName = "",
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -12,21 +19,56 @@ class LoadingError extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            "Failed to load data. Please try again.",
-            style: TextStyle(
-              fontSize: 16,
+          RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+              style: const TextStyle(
+                fontSize: 16,
+                height: 1.8,
+                color: Colors.black, // Color of the whole text
+              ),
+              children: [
+                TextSpan(
+                  text: isNoData
+                      ? "No data found for "
+                      : "Failed to load data. Please try again.",
+                ),
+                if (isNoData)
+                  TextSpan(
+                    text: artistName,
+                    style: const TextStyle(
+                      color: Colors.red, // Color of $artistName
+                    ),
+                  ),
+                if (isNoData)
+                  const TextSpan(
+                    text: "\nPlease try searching for another artist",
+                  ),
+              ],
             ),
           ),
-          const SizedBox(height: 10),
-          OutlinedButton(
-            style: OutlinedButton.styleFrom(
-              foregroundColor: Colors.red,
-              side: const BorderSide(color: Colors.red),
-            ),
-            onPressed: onRetry,
-            child: const Text("Retry"),
-          ),
+          SizedBox(height: !isNoData ? 10 : 3),
+          if (!isNoData)
+            // Retry button
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.red,
+                side: const BorderSide(color: Colors.red),
+              ),
+              onPressed: onRetry,
+              child: const Text("Retry"),
+            )
+          else
+            // Back button
+            TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.blue,
+                // Turn off on long press effect
+                overlayColor: Colors.transparent,
+              ),
+              onPressed: () => Navigator.pop(context),
+              child: const Text("Back"),
+            )
         ],
       ),
     );
