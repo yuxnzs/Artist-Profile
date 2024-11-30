758444d01fb7a0256a4daee361065d65556d7171
yuxnzs <yuchengliu0429@gmail.com>
2024-11-30 19:02:14 +0800

    Add global_tab_controller to handle navbar index changes in MainNavigation and HomepageBanner

---
+++ lib/utility/global_tab_controller.dart
@@ -0,0 +1,5 @@
+import "package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart";
+
+// Global controller for MainNavigation and HomepageBanner to control the tab index
+PersistentTabController globalTabController =
+    PersistentTabController(initialIndex: 0);
