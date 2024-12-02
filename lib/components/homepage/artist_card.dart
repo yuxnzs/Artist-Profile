c99f7ed7dbc265e193afd46daf422890999a2eb1
yuxnzs <yuchengliu0429@gmail.com>
2024-12-02 23:58:50 +0800

    Replace default `...` with half-transparent filter for long artist names to improve UX in ArtistCard

--- lib/components/homepage/artist_card.dart
+++ lib/components/homepage/artist_card.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:intl/intl.dart';
+import 'package:intl/intl.dart'
+    as intl; // Avoid conflict with Flutter's TextDirection
 import 'package:artist_profile/models/artist.dart';
 import 'package:artist_profile/utility/custom_colors.dart';
 import 'package:artist_profile/components/homepage/rounded_artist_image.dart';
 import 'package:artist_profile/pages/artist_bio_page.dart';
 
-class ArtistCard extends StatelessWidget {
+class ArtistCard extends StatefulWidget {
   final Artist artist;
   // For Hero() tag parameter
   final String category;
@@ -18,6 +19,42 @@ class ArtistCard extends StatelessWidget {
   });
 
   @override
+  State<ArtistCard> createState() => _ArtistCardState();
+}
+
+class _ArtistCardState extends State<ArtistCard> {
+  bool isTextTooLong = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _checkTextWidth();
+    });
+  }
+
+  void _checkTextWidth() {
+    final textPainter = TextPainter(
+      text: TextSpan(
+        text: widget.artist.name,
+        style: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      maxLines: 1,
+      textDirection: TextDirection.ltr,
+    );
+
+    textPainter.layout(maxWidth: 140); // 160 width - 20 padding
+    if (textPainter.didExceedMaxLines) {
+      setState(() {
+        isTextTooLong = true;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
@@ -25,10 +62,10 @@ class ArtistCard extends StatelessWidget {
           context,
           CupertinoPageRoute(
             builder: (context) => ArtistBioPage(
-              artistName: artist.name,
+              artistName: widget.artist.name,
               apiIncludeSpotifyInfo: false,
-              passedArtist: artist,
-              category: category,
+              passedArtist: widget.artist,
+              category: widget.category,
             ),
           ),
         );
@@ -46,22 +83,52 @@ class ArtistCard extends StatelessWidget {
           children: [
             // Artist Image
             Hero(
-              tag: "${artist.name}-$category",
-              child: RoundedArtistImage(imageUrl: artist.image ?? ""),
+              tag: "${widget.artist.name}-${widget.category}",
+              child: RoundedArtistImage(imageUrl: widget.artist.image ?? ""),
             ),
             const SizedBox(height: 10),
 
             // Artist Name
-            Text(
-              artist.name,
-              textAlign: TextAlign.center,
-              overflow: TextOverflow.ellipsis,
-              maxLines: 1,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+            // ClipRRect hides overflowed text
+            ClipRRect(
+              // Use Stack to create a gradient effect on the long artist name
+              child: Stack(
+                children: [
+                  Text(
+                    widget.artist.name,
+                    maxLines: 1,
+                    softWrap: false, // To prevent text from going to next line
+                    overflow: TextOverflow.visible,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  if (isTextTooLong)
+                    // Gradient effect on the long artist name
+                    Positioned.fill(
+                      child: Align(
+                        alignment: Alignment.bottomRight,
+                        child: Container(
+                          width: 40,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                CustomColors.background.withOpacity(0.1),
+                                CustomColors.background,
+                              ],
+                              stops: const [0.5, 0.9],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                ],
               ),
             ),
+
             const SizedBox(height: 3),
 
             // Followers
@@ -75,7 +142,7 @@ class ArtistCard extends StatelessWidget {
                 ),
                 const SizedBox(width: 3),
                 Text(
-                  NumberFormat.compact().format(artist.followers),
+                  intl.NumberFormat.compact().format(widget.artist.followers),
                   style: const TextStyle(
                     fontSize: 14,
                     fontWeight: FontWeight.bold,
