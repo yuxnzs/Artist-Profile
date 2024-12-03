a3bfc8072f61e84371635d45a1e5fb97ed04ba92
yuxnzs <yuchengliu0429@gmail.com>
2024-12-04 01:12:51 +0800

    Update LoadingError: Adjust height to 225, add background color, and add displayBackground parameter

--- lib/components/common/loading_error.dart
+++ lib/components/common/loading_error.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:artist_profile/utility/app_constants.dart';
+import 'package:artist_profile/utility/custom_colors.dart';
 
 class LoadingError extends StatelessWidget {
   final Function() onRetry;
   final bool isNoData;
   final String artistName;
+  final bool displayBackground;
 
   const LoadingError({
     super.key,
     required this.onRetry,
     this.isNoData = false,
     this.artistName = "",
+    this.displayBackground = true,
   });
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 180,
+    return Container(
+      height: 225, // Approximate height of SectionPlaceholder
+      width: double.infinity,
+      margin: const EdgeInsets.symmetric(horizontal: AppConstants.globalMargin),
+      decoration: BoxDecoration(
+        color: displayBackground ? CustomColors.background : Colors.transparent,
+        borderRadius: BorderRadius.circular(10),
+      ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
