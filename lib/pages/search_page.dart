69937aea8cce4ef202f91cebf78c88a7c8ea51e4
yuxnzs <yuchengliu0429@gmail.com>
2024-12-07 06:42:35 +0800

    Show rate-limiting notification if searching more than once within 5 seconds in SearchPage

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -6,15 +6,23 @@ import 'package:artist_profile/components/search_page/keyboard_dismissible_scrol
 import 'package:artist_profile/components/search_page/search_text_field.dart';
 import 'package:artist_profile/components/search_page/search_history_tile.dart';
 import 'package:artist_profile/managers/search_history_manager.dart';
+import 'package:artist_profile/managers/notification_manager.dart';
 
 class SearchPage extends StatefulWidget {
-  const SearchPage({super.key});
+  final bool isKeyboardVisible;
+
+  const SearchPage({super.key, required this.isKeyboardVisible});
+
   @override
   State<SearchPage> createState() => _SearchPageState();
 }
 
 class _SearchPageState extends State<SearchPage> with WidgetsBindingObserver {
+  final NotificationManager _notificationManager = NotificationManager();
   final TextEditingController _searchController = TextEditingController();
+
+  bool canFetchBio = true; // To prevent the user from searching too frequently
+  int waitTime = 5000;
 
   @override
   void initState() {
@@ -28,6 +36,34 @@ class _SearchPageState extends State<SearchPage> with WidgetsBindingObserver {
   void dispose() {
     _searchController.dispose();
     super.dispose();
+  }
+
+  void setCanFetchTimeout() {
+    setState(() {
+      canFetchBio = false;
+    });
+
+    Future.delayed(Duration(milliseconds: waitTime), () {
+      if (mounted) {
+        setState(() {
+          canFetchBio = true;
+        });
+      }
+    });
+  }
+
+  void _showNotification() {
+    _notificationManager.showNotification(
+      context: context,
+      message: "Please wait ${waitTime ~/ 1000} seconds to search again",
+      duration: 5,
+      isSlideHorizontal: true,
+    );
+  }
+
+  void _hideNotification() {
+    // Animate the notification bar to hide and remove it
+    _notificationManager.hideNotificationBar();
   }
 
   @override
@@ -69,31 +105,50 @@ class _SearchPageState extends State<SearchPage> with WidgetsBindingObserver {
                             searchHistoryManager.deleteHistory(index);
                           },
                           onTap: (searchText) {
-                            // Close the keyboard
-                            FocusScope.of(context).unfocus();
+                            if (canFetchBio) {
+                              // If there is a notification showing, remove it first before navigating to the ArtistBioPage
+                              _hideNotification();
 
-                            // Navigate to the artist bio page
-                            Navigator.push(
-                              context,
-                              CupertinoPageRoute(
-                                builder: (context) => ArtistBioPage(
-                                  artistName: searchText,
-                                  apiIncludeSpotifyInfo: true,
-                                  passedArtist: null,
-                                  category: null,
+                              // Close the keyboard
+                              FocusScope.of(context).unfocus();
+
+                              // Navigate to the artist bio page
+                              Navigator.push(
+                                context,
+                                CupertinoPageRoute(
+                                  builder: (context) => ArtistBioPage(
+                                    artistName: searchText,
+                                    apiIncludeSpotifyInfo: true,
+                                    passedArtist: null,
+                                    category: null,
+                                  ),
                                 ),
-                              ),
-                            );
-
-                            // Update the timestamp of the search history to display it at the top
-                            // Delay 1 second to navigate to the ArtistBioPage first, then update the timestamp for UX
-                            Future.delayed(const Duration(seconds: 1), () {
-                              searchHistoryManager.updateTimestamp(
-                                index: index,
-                                timestamp:
-                                    DateTime.now().millisecondsSinceEpoch,
                               );
-                            });
+
+                              // Update the timestamp of the search history to display it at the top
+                              // Delay 1 second to navigate to the ArtistBioPage first, then update the timestamp for UX
+                              Future.delayed(const Duration(seconds: 1), () {
+                                searchHistoryManager.updateTimestamp(
+                                  index: index,
+                                  timestamp:
+                                      DateTime.now().millisecondsSinceEpoch,
+                                );
+                              });
+
+                              setCanFetchTimeout();
+                            } else {
+                              // Close the keyboard first
+                              FocusScope.of(context).unfocus();
+
+                              // If the keyboard is visible, wait for 500ms to close the keyboard before showing notification
+                              Future.delayed(
+                                  Duration(
+                                      milliseconds: widget.isKeyboardVisible
+                                          ? 400
+                                          : 0), () {
+                                _showNotification();
+                              });
+                            }
                           },
                         );
                       },
@@ -109,25 +164,43 @@ class _SearchPageState extends State<SearchPage> with WidgetsBindingObserver {
               width: screenWidth,
               onSubmitted: (value) {
                 if (value.isNotEmpty) {
-                  // Navigate to the artist bio page first
-                  Navigator.push(
-                    context,
-                    CupertinoPageRoute(
-                      builder: (_) => ArtistBioPage(
-                        artistName: value,
-                        apiIncludeSpotifyInfo: true,
-                        passedArtist: null,
-                        category: null,
-                      ),
-                    ),
-                  );
+                  if (canFetchBio) {
+                    // If there is a notification showing, remove it first before navigating to the ArtistBioPage
+                    _hideNotification();
 
-                  Future.delayed(const Duration(seconds: 1), () {
-                    // Then add the search history to the database
-                    searchHistoryManager.addHistory(value);
-                    // Clear the search text field
-                    _searchController.clear();
-                  });
+                    // Navigate to the artist bio page first
+                    Navigator.push(
+                      context,
+                      CupertinoPageRoute(
+                        builder: (_) => ArtistBioPage(
+                          artistName: value,
+                          apiIncludeSpotifyInfo: true,
+                          passedArtist: null,
+                          category: null,
+                        ),
+                      ),
+                    );
+
+                    Future.delayed(const Duration(seconds: 1), () {
+                      // Then add the search history to the database
+                      searchHistoryManager.addHistory(value);
+                      // Clear the search text field
+                      _searchController.clear();
+                    });
+
+                    setCanFetchTimeout();
+                  } else {
+                    // Close the keyboard first
+                    FocusScope.of(context).unfocus();
+
+                    // If the keyboard is visible, wait for 500ms to close the keyboard before showing notification
+                    Future.delayed(
+                        Duration(
+                            milliseconds: widget.isKeyboardVisible ? 400 : 0),
+                        () {
+                      _showNotification();
+                    });
+                  }
                 }
               },
             ),
