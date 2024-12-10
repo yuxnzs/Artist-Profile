ff30a031f05158660d0a0214cd62b0cd2520c1ac
yuxnzs <yuchengliu0429@gmail.com>
2024-12-11 01:57:50 +0800

    Register Hive adapter and inject DisplayManager into APIService through context in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:artist_profile/services/api_service.dart';
 import 'package:artist_profile/managers/display_manager.dart';
 import 'package:artist_profile/pages/main_navigation.dart';
 import 'package:artist_profile/managers/search_history_manager.dart';
+import 'package:artist_profile/models/artist_hive.dart';
+import 'package:artist_profile/models/artist_bio_hive.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+  await Hive.initFlutter();
+  Hive.registerAdapter(ArtistBioHiveAdapter());
+  Hive.registerAdapter(ArtistHiveAdapter());
   runApp(const MyApp());
 }
 
@@ -18,8 +25,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => APIService()),
         ChangeNotifierProvider(create: (context) => DisplayManager()),
+        ChangeNotifierProvider(
+            create: (context) => APIService(context.read<DisplayManager>())),
         ChangeNotifierProvider(
             // Fetch history when the app starts
             create: (context) => SearchHistoryManager()..fetchHistory()),
